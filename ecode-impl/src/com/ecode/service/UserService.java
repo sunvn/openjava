aa5de19d4f91e7cb51749d82f8cf213f35255f50
Nguyen van Ha <hanv9@vpbank.com.vn>
2017-12-04 13:46:09 +0700

    first commit

--- ecode-impl/src/com/ecode/service/UserService.java
+++ ecode-impl/src/com/ecode/service/UserService.java
@@ -5,7 +5,7 @@ public class UserService extends ServiceAbstract implements TransactionService {
   private static UserService instance = null;
 
   private UserService() {
-    // Exists only to defeat instantiation.
+   
   }
 
   public static UserService getInstance() {
